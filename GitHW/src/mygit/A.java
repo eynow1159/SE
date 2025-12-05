af43ff2de73dc873a10349bc8ef83aa621f3ef53
eynow <eynow@EYNOW>
2025-12-05 23:40:00 +0900

    Bob이 변경

--- GitHW/src/mygit/A.java
+++ GitHW/src/mygit/A.java
@@ -2,4 +2,9 @@ package mygit;
 
 public class A {
 
+	public void sayHi(String toWhom) {
+		B b = new B();
+		b.sayHi(toWhom);
+	}
+	
 }
--- GitHW/src/mygit/B.java
+++ GitHW/src/mygit/B.java
@@ -2,4 +2,8 @@ package mygit;
 
 public class B {
 
+	public void sayHi(String toWhom) {
+		System.out.println("Hi! " + toWhom);
+	}
+
 }
